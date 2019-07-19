98acca06ac06e881d9aae4438ee047b1dbc055d3
Shivam <jalotrashivam9@gmail.com>
2019-07-19 17:11:08 +0530

    Changed some inner workings

--- src/mainprogram/mainexecution.go
+++ src/mainprogram/mainexecution.go
@@ -3,10 +3,15 @@ package main
 import (
 	"downloadfiles"
 	"fmt"
+	"geturldata"
 	"os"
 )
 
-func main() {
+func parseWEBSITEADDRESS() {
+	geturldata.ReturnURLBODY(geturldata.DownloadWEBADDRESS())
+}
+
+func downloaderASSEMBLY() {
 	if len(os.Args) != 3 {
 		fmt.Println("usage: download url filename")
 		os.Exit(1)
@@ -15,15 +20,22 @@ func main() {
 	filename := os.Args[2]
 
 	// Changing filename to filepath so that the songs are downloaded
-	// in separate folder Songs.
+	// in separate folder Songs and then in the albums folder .
 
 	os.Chdir("../")
 	os.Mkdir("songs", 777)
 	os.Chdir("./songs")
+	os.Mkdir(geturldata.ParseWEBSITEADDRESS(os.Args[1]), 777)
+	os.Chdir("./" + geturldata.ParseWEBSITEADDRESS(os.Args[1]))
 
 	err := downloadfiles.DownloadFile(url, filename)
 	if err != nil {
 		panic(err)
 	}
 
+}
+
+func main() {
+	// fmt.Println(geturldata.DownloadWEBADDRESS())
+	parseWEBSITEADDRESS()
 }
